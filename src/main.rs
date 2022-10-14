f7a8af3e12b23479224ae8f59da13fb34fe708a2
Sangchual CHA <scha@swidch.com>
2022-10-14 19:05:08 +0100

    fix typo in type_conversion example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod hello_world;
 mod display_csv;
 mod data_type_conversion_awkwardness;
+mod type_case_with_try_into;
+mod while_with_time;
+mod mandelbrot;
 
 fn main() {
     // display_csv::display_csv();
-    data_type_conversion_awkwardness::conv_i32_to_i8();
+    // data_type_conversion_awkwardness::conv_i32_to_i8();
+    // type_case_with_try_into::type_case_with_try_into();
+    // while_with_time::loop_in_time();
+    mandelbrot::draw_madelbrot();
 }
