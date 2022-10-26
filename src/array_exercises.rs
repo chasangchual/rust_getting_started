9f3bc6f35c853b8ce584fe952281b5dc5771f2a6
Sangchual CHA <scha@swidch.com>
2022-10-26 18:34:21 +0100

    array example codes

---
+++ src/array_exercises.rs
@@ -0,0 +1,35 @@
+pub fn run_array_example() {
+    let one = [1, 2, 3];
+    let two: [u8; 3] = [1, 2, 3]; // declare type of the array, [u8; 3] => 3 element of u8 type
+    let three = [9; 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3]; // declared length (3, in [u8; 3]) should match with the actual assigned length of the array (3 in [0; 3])
+
+    println!("{:?}", one); // [1, 2, 3]
+    println!("{:?}", two); // [1, 2, 3]
+    println!("{:?}", blank1); // [0, 0, 0]
+    println!("{:?}", blank2); // [0, 0, 0]
+
+    let arrays = [one, two, three, blank1, blank2];
+
+    for a in arrays {
+        print!("{:?}", a); // [1, 2, 3]
+        for e in a {
+            print!("\t{} + 10 = {}\t", e, e + 10);
+        }
+
+        let mut s = 0;
+        for i in 0..a.len() {
+            s += a[i];
+        }
+        print!("\tsum of {:?} = {}", a, s);
+        println!();
+    }
+    /*
+        [1, 2, 3]	1 + 10 = 11		2 + 10 = 12		3 + 10 = 13		sum of [1, 2, 3] = 6
+        [1, 2, 3]	1 + 10 = 11		2 + 10 = 12		3 + 10 = 13		sum of [1, 2, 3] = 6
+        [9, 9, 9]	9 + 10 = 19		9 + 10 = 19		9 + 10 = 19		sum of [9, 9, 9] = 27
+        [0, 0, 0]	0 + 10 = 10		0 + 10 = 10		0 + 10 = 10		sum of [0, 0, 0] = 0
+        [0, 0, 0]	0 + 10 = 10		0 + 10 = 10		0 + 10 = 10		sum of [0, 0, 0] = 0
+     */
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod type_case_with_try_into;
 mod while_with_time;
 mod mandelbrot;
 mod life_time_basic;
+mod array_exercises;
 
 fn main() {
     // display_csv::display_csv();
@@ -12,5 +13,6 @@ fn main() {
     // type_case_with_try_into::type_case_with_try_into();
     // while_with_time::loop_in_time();
     // mandelbrot::draw_madelbrot();
-    life_time_basic::run_find_longest();
+    // life_time_basic::run_find_longest();
+    array_exercises::run_array_example();
 }
